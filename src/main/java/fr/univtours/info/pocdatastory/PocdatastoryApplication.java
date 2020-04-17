0c2621504ac4cb9b63cb8ceb3e3a976e5c6e7ed3
marcel <marcel@iuniv-tours.fr>
2020-04-17 09:23:17 +0200

    first commit

---
+++ src/main/java/fr/univtours/info/pocdatastory/PocdatastoryApplication.java
@@ -0,0 +1,13 @@
+package fr.univtours.info.pocdatastory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocdatastoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocdatastoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/univtours/info/pocdatastory/PocdatastoryApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univtours.info.pocdatastory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PocdatastoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
