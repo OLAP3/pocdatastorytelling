82663488f1779dffd379fd41445a8107fb63d512
marcel <marcel@iuniv-tours.fr>
2020-05-23 18:31:39 +0200

    preparing for worldmap bubble chart

--- src/main/java/fr/univtours/info/simpleStory/SimpleSQLvizDashboardComponent.java
+++ src/main/java/fr/univtours/info/simpleStory/SimpleSQLvizDashboardComponent.java
@@ -12,9 +12,17 @@ import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.axis.NumberAxis;
+import org.jfree.chart.plot.Plot;
 import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.chart.plot.XYPlot;
+import org.jfree.chart.renderer.xy.XYItemRenderer;
 import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.data.xy.DefaultXYZDataset;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
 import java.io.File;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -56,7 +64,7 @@ public class SimpleSQLvizDashboardComponent implements DashboardComponent {
                 }
 
 
-                toBarChartImage();
+                toImage();
                 if(filename!=null){
                     contentStream.endText();
                     contentStream.close();
@@ -107,13 +115,29 @@ public class SimpleSQLvizDashboardComponent implements DashboardComponent {
         this.contentStream=contentStream;
     }
 
+    // measures should be first
+    public void toImage() throws Exception{
+        this.metaData = resultSet.getMetaData();
+        /*
+        if(metaData.getColumnCount() == 3){
+            toLineChart();
+        }
+
+         */
+        if (metaData.getColumnCount() == 3) {
+            toBarChartImage();
+        }
+        if (metaData.getColumnCount() == 2) {
+            toPieChartImage();
+        }
+        if (metaData.getColumnCount() == 4) {
+            toWorldMapImage();
+        }
+    }
 
 
     public void toBarChartImage() throws Exception{
 
-            this.metaData = resultSet.getMetaData();
-
-            if (metaData.getColumnCount() == 3) {
                 rangeAxisLabel = metaData.getColumnName(1);
                 domainAxisLabel = metaData.getColumnName(3);
 
@@ -136,18 +160,133 @@ public class SimpleSQLvizDashboardComponent implements DashboardComponent {
                         dataset, PlotOrientation.VERTICAL,
                         true, true, false);
 
+                Plot plot = barChart.getPlot();
+                plot.setBackgroundPaint(Color.WHITE);
+
+
                 int width = 640;    /* Width of the image */
                 int height = 480;   /* Height of the image */
-                filename = "public/img/BarChart-" + nbCharts + ".jpeg";
+                filename = "public/img/BarChart-" + nbCharts + ".png";
                 File BarChart = new File(filename);
 
-                ChartUtilities.saveChartAsJPEG(BarChart, barChart, width, height);
+                ChartUtilities.saveChartAsPNG(BarChart, barChart, width, height);
                 this.nbCharts++;
-            }
+
+    }
 
 
 
 
+
+    public void toWorldMapImage() throws Exception {
+
+        DefaultXYZDataset defaultxyzdataset = new DefaultXYZDataset( );
+        //double ad[ ] = { 30 , 40 , 50 , 60 , 70 , 80 };
+        //double ad1[ ] = { 10 , 20 , 30 , 40 , 50 , 60 };
+        //double ad2[ ] = { 4 , 5 , 10 , 8 , 9 , 6 };
+        //double ad3[ ][ ] = { ad , ad1 , ad2 };
+        //defaultxyzdataset.addSeries( "Series 1" , ad3 );
+
+
+
+        resultSet.last();    // moves cursor to the last row
+        int size = resultSet.getRow();
+        resultSet.beforeFirst();
+        double cases[] = new double[size];
+        double longitude[] = new double[size];
+        double latitude[] = new double[size];
+        int i=0;
+        ResultSetIterator rsit = new ResultSetIterator(resultSet);
+        while (rsit.hasNext()) {
+            Object[] tab = rsit.next();
+            cases[i]=Double.parseDouble(tab[0].toString())/100000; // /100000 for scaling
+            longitude[i]=Double.parseDouble(tab[2].toString());
+            latitude[i]=Double.parseDouble(tab[3].toString());
+            i++;
+        }
+
+        double ad3[ ][ ] = { longitude , latitude, cases };
+        defaultxyzdataset.addSeries( metaData.getColumnName(1) , ad3 );
+
+        JFreeChart bubblechart = ChartFactory.createBubbleChart(
+                "",
+                "",
+                "",
+                defaultxyzdataset,
+                PlotOrientation.HORIZONTAL,
+                true, true, false);
+
+        XYPlot xyplot = ( XYPlot )bubblechart.getPlot( );
+        //xyplot.setForegroundAlpha( 0.65F );
+        XYItemRenderer xyitemrenderer = xyplot.getRenderer( );
+        xyitemrenderer.setSeriesPaint( 0 , Color.red );
+        //NumberAxis numberaxis = ( NumberAxis )xyplot.getDomainAxis( );
+        //numberaxis.setLowerMargin( 0.2 );
+        //numberaxis.setUpperMargin( 0.5 );
+        //NumberAxis numberaxis1 = ( NumberAxis )xyplot.getRangeAxis( );
+        //numberaxis1.setLowerMargin( 0.8 );
+        //numberaxis1.setUpperMargin( 0.9 );
+
+        Image icon = ImageIO.read(new File("public/img/map.gif"));
+        bubblechart.setBackgroundImage(icon);
+
+        //axis.setVisible(false);
+
+        //xyplot.setBackgroundImage(icon);
+        bubblechart.removeLegend();
+        bubblechart.setBackgroundImage(icon);
+        Color trans = new Color(0xFF, 0xFF, 0xFF, 0);
+        xyplot.setBackgroundPaint( trans );
+        xyplot.setOutlineVisible(false);
+
+        //plot.setRangeGridlinesVisible(false);
+        //plot.setDomainGridlinesVisible(false);
+
+        int width = 800;    /* Width of the image */
+        int height = 500;   /* Height of the image */
+        filename = "public/img/BarChart-" + nbCharts + ".png";
+        File BarChart = new File(filename);
+
+        ChartUtilities.saveChartAsPNG(BarChart, bubblechart, width, height);
+        this.nbCharts++;
+
+
+
+    }
+
+
+        public void toPieChartImage() throws Exception{
+
+        rangeAxisLabel = metaData.getColumnName(1);
+        domainAxisLabel = metaData.getColumnName(2);
+
+        final DefaultPieDataset dataset = new DefaultPieDataset( );
+        //dataset.addValue( 1.0 , fiat , speed );
+
+        resultSet.beforeFirst();
+
+        ResultSetIterator rsit = new ResultSetIterator(resultSet);
+        while (rsit.hasNext()) {
+            Object[] tab = rsit.next();
+           // System.out.println(Double.parseDouble(tab[0].toString()) +  tab[1].toString() + tab[2].toString());
+            dataset.setValue(tab[1].toString(), Double.parseDouble(tab[0].toString()));
+        }
+
+
+        JFreeChart barChart = ChartFactory.createPieChart(
+                "",
+              //  domainAxisLabel, rangeAxisLabel,
+                dataset,
+                //PlotOrientation.VERTICAL,
+                true, true, false);
+
+        int width = 640;    /* Width of the image */
+        int height = 480;   /* Height of the image */
+        filename = "public/img/PieChart-" + nbCharts + ".png";
+        File pieChart = new File(filename);
+
+        ChartUtilities.saveChartAsPNG(pieChart, barChart, width, height);
+        this.nbCharts++;
 
     }
 
